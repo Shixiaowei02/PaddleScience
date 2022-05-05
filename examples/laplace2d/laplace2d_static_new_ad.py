6dc5a5c9a6c757d9e23cdc120a012834e39f4878
jiangcheng <thisjiang@qq.com>
2022-05-05 20:33:55 +0800

    update hidden size from 20 to 50 and update cinn optimize code

--- examples/laplace2d/laplace2d_static_new_ad.py
+++ examples/laplace2d/laplace2d_static_new_ad.py
@@ -15,6 +15,7 @@
 import paddle
 import time
 import six
+import os
 import paddlescience as psci
 import paddle.compat as cpt
 import numpy as np
@@ -28,6 +29,132 @@ paddle.enable_static()
 paddle.seed(1234)
 np.random.seed(1234)
 enable_prim()
+
+convert_back_to_program = (os.getenv('FLAGS_use_cinn') != "1")
+
+
+def cinn_optimize_program(program):
+    def _remove_unused_var(program):
+        all_remove_vars = []
+        for block in program.blocks:
+            args = []
+            for op in block.ops:
+                args += op.input_arg_names
+                args += op.output_arg_names
+            args = list(set(args))  # vals of all left ops
+            var_names = block.vars.keys()  # all vals
+            sub_block_remove_vars = []
+            for var in var_names:
+                if var not in args:
+                    sub_block_remove_vars.append(var)
+            all_remove_vars.append(sub_block_remove_vars)
+
+        remove_vars = [list(set(v)) for v in all_remove_vars]
+        for i, block in enumerate(program.blocks):
+            for v in remove_vars[i]:
+                block._remove_var(v)
+
+    def dead_code_elimination(program):
+        program._sync_with_cpp()
+        all_input_arg_names = set()
+        for block in program.blocks:
+            ops = list(block.ops)
+            for op in ops:
+                for name in op.input_arg_names:
+                    all_input_arg_names.add(name)
+
+        for block in program.blocks:
+            ops = list(block.ops)
+            for op in ops:
+                if op.type == "fill_constant_p" and (
+                        op.output('Y')[0] not in all_input_arg_names):
+                    idx = block.ops.index(op)
+                    block._remove_op(idx)
+
+        _remove_unused_var(program)
+        program._sync_with_cpp()
+
+    def fuse_shape_fill_constant(program):
+        def _insert_fill_any_like_op(block, index, shape_op, fill_constant_op):
+            fill_any_like_inputs = {}
+            fill_any_like_inputs['X'] = block.var(shape_op.input('Input')[0])
+            fill_any_like_outputs = {}
+            fill_any_like_outputs['Out'] = block.var(
+                fill_constant_op.output('Out')[0])
+            fill_any_like_attrs = {}
+            fill_any_like_attrs['value'] = fill_constant_op.attr('value')
+            fill_any_like_attrs['dtype'] = fill_constant_op.attr('dtype')
+            fill_any_like_attrs['op_role'] = fill_constant_op.attr('op_role')
+
+            fill_any_like_op = block._insert_op(
+                index,
+                type='fill_any_like',
+                inputs=fill_any_like_inputs,
+                outputs=fill_any_like_outputs,
+                attrs=fill_any_like_attrs)
+            return fill_any_like_op
+
+        program._sync_with_cpp()
+        block = program.block(0)
+        i = 0
+        while i < len(block.ops):
+            # find a fill_constant op
+            if block.ops[i].type == 'fill_constant':
+                fill_constant_op = block.ops[i]
+                fill_constant_idx = i
+                shape_idx = -1
+                # find the preceding shape op
+                for j in reversed(range(fill_constant_idx)):
+                    if block.ops[j].type == 'shape':
+                        shape_out_name = block.ops[j].output_arg_names[0]
+                        if shape_out_name in fill_constant_op.input_arg_names:
+                            shape_op = block.ops[j]
+                            shape_idx = j
+                            break
+                if shape_idx < 0:
+                    i += 1
+                    continue
+                # create and insert a new fill_any_like op
+                _insert_fill_any_like_op(block, fill_constant_idx + 1,
+                                         shape_op, fill_constant_op)
+                # remove the old operators
+                block._remove_op(fill_constant_idx)
+                block._remove_op(shape_idx)
+                # restart scanning for elementwise add from the deleted shape's index
+                i = shape_idx
+            i += 1
+        _remove_unused_var(program)
+        program._sync_with_cpp()
+
+    dead_code_elimination(train_program)
+    fuse_shape_fill_constant(train_program)
+
+
+def add_fetch_ops(program, fetch_list, fetch_var_name='fetch'):
+    assert isinstance(program, fluid.Program)
+    global_block = program.global_block()
+
+    if fetch_var_name in global_block.vars:
+        fetch_var = global_block.var(fetch_var_name)
+    else:
+        fetch_var = global_block.create_var(
+            name=fetch_var_name,
+            type=core.VarDesc.VarType.FETCH_LIST,
+            persistable=True)
+
+    # append fetch_operators
+    if not fluid.executor.has_fetch_operators(global_block, fetch_list,
+                                              fetch_var_name, 'fetch'):
+        for i, var in enumerate(fetch_list):
+            assert isinstance(var, Variable) or isinstance(
+                var, six.string_types), ("Wrong type for fetch_list[%s]: %s" %
+                                         (i, type(var)))
+            global_block.append_op(
+                type='fetch',
+                inputs={'X': [var]},
+                outputs={'Out': [fetch_var]},
+                attrs={'col': i})
+    program._sync_with_cpp()
 
 
 def compile(program, loss_name=None):
@@ -106,7 +233,8 @@ def compile_and_convert_back_to_program(program=None,
         raise ValueError("Unsupport to fetch optimize OP.")
 
     if use_prune:
-        program = executor._prune_program(program, feed, fetch_list, optimize_ops)
+        program = executor._prune_program(program, feed, fetch_list,
+                                          optimize_ops)
         feed = executor._update_feed(program, feed)
 
     program_with_fetch_op = _add_fetch_ops(program, fetch_list, fetch_var_name)
@@ -175,7 +303,7 @@ with paddle.static.program_guard(train_program, startup_program):
         num_ins=2,
         num_outs=1,
         num_layers=10,
-        hidden_size=20,
+        hidden_size=50,
         dtype='float32',
         activation='tanh')
 
@@ -196,7 +324,7 @@ with paddle.static.program_guard(train_program, startup_program):
 
     loss = bc_loss + eq_loss
     paddle.fluid.optimizer.AdamOptimizer(0.001).minimize(loss)
-    if prim_enabled():
+    if convert_back_to_program and prim_enabled():
         prim2orig(inputs.block)
 
 # print('startup_program: ', startup_program)
@@ -206,10 +334,10 @@ exe.run(startup_program)
 num_epoch = 10000
 
 feeds = {
-            'x': geo.get_space_domain().astype(np.float32),
-            'bc_idx': geo.bc_index.astype(np.int32),
-            'bc_v': pdes.bc_value
-        }
+    'x': geo.get_space_domain().astype(np.float32),
+    'bc_idx': geo.bc_index.astype(np.int32),
+    'bc_v': pdes.bc_value
+}
 fetchs = [loss.name, eq_loss.name, bc_loss.name, outputs.name]
 
 convert_back_to_program = True
@@ -221,16 +349,18 @@ if convert_back_to_program:
         use_prune=True,
         loss_name=loss.name)
 else:
+    # use CINN
+    cinn_optimize_program(train_program)
+    add_fetch_ops(train_program, fetch_list=fetchs)
     compiled_program = compile(train_program, loss.name)
 
 print("Get train program successfully, congratulations !!!")
 
 begin = time.time()
 for i in range(num_epoch):
-    loss_d, eq_loss_d, bc_loss_d, outputs_d = exe.run(
-        train_program,
-        feed=feeds,
-        fetch_list=fetchs)
+    loss_d, eq_loss_d, bc_loss_d, outputs_d = exe.run(compiled_program,
+                                                      feed=feeds,
+                                                      fetch_list=fetchs)
     print('num_epoch: ', i, '/', num_epoch, ' loss: ', loss_d[0], ' eq_loss: ',
           eq_loss_d[0], 'bc_loss: ', bc_loss_d[0], 'outputs[0][0]: ',
           outputs_d[0][0])
@@ -238,7 +368,7 @@ for i in range(num_epoch):
 end = time.time()
 print('10000 epoches time: ', end - begin)
 
-rslt = exe.run(train_program,
+rslt = exe.run(compiled_program,
                feed={
                    'x': geo.get_space_domain().astype(np.float32),
                    'bc_idx': geo.bc_index.astype(np.int32),
